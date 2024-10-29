44a0395a76cd2ab8670be79537d1fca8ce93a9eb
Jolene Le <38019858+jolenele@users.noreply.github.com>
2024-10-29 00:37:55 -0700

    Add OpenCV

--- app/src/main/java/com/example/npl_object_detection_medical_bills/MainActivity.kt
+++ app/src/main/java/com/example/npl_object_detection_medical_bills/MainActivity.kt
@@ -1,20 +1,32 @@
 package com.example.npl_object_detection_medical_bills
 
 import android.os.Bundle
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import org.opencv.android.OpenCVLoader
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var textViewStatus: TextView
+    private var isOpenCvInitialized = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        textViewStatus = findViewById(R.id.textViewStatus)
+
+        isOpenCvInitialized = OpenCVLoader.initLocal()
+
+        updateControls()
+    }
+
+    private fun updateControls() {
+        if(!isOpenCvInitialized) {
+            textViewStatus.text = "Image Preprocess Error"
+        } else {
+            textViewStatus.text = "Image Preprocess"
         }
     }
 }
