7507f869c266e5258e8f6e556ee0367b30de7ef9
Jolene Le <38019858+jolenele@users.noreply.github.com>
2024-10-29 00:47:28 -0700

    Add camera - Request camera access

--- app/src/main/java/com/example/npl_object_detection_medical_bills/MainActivity.kt
+++ app/src/main/java/com/example/npl_object_detection_medical_bills/MainActivity.kt
@@ -6,9 +6,31 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import org.opencv.android.OpenCVLoader
 
-class MainActivity : AppCompatActivity() {
+import android.Manifest
+import android.content.pm.PackageManager
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+
+import org.opencv.android.CameraBridgeViewBase
+import android.widget.Button
+import android.widget.CheckBox
+import android.widget.ImageView
+import org.opencv.core.CvType
+import org.opencv.core.Mat
+
+class MainActivity : AppCompatActivity(), CameraBridgeViewBase.CvCameraViewListener2 {
     private lateinit var textViewStatus: TextView
     private var isOpenCvInitialized = false
+
+    private val cameraPermissionRequestCode = 100
+
+    private lateinit var buttonStartPreview: Button
+    private lateinit var buttonStopPreview: Button
+    private lateinit var checkBoxProcessing: CheckBox
+    private lateinit var imageView: ImageView
+    private lateinit var openCvCameraView: CameraBridgeViewBase
+
+    private var isPreviewActive = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,6 +40,17 @@ class MainActivity : AppCompatActivity() {
         textViewStatus = findViewById(R.id.textViewStatus)
 
         isOpenCvInitialized = OpenCVLoader.initLocal()
+
+        // Request access to camera
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA)
+            != PackageManager.PERMISSION_GRANTED) {
+            // Permission is not granted, request it
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(Manifest.permission.CAMERA),
+                cameraPermissionRequestCode
+            )
+        }
 
         updateControls()
     }
