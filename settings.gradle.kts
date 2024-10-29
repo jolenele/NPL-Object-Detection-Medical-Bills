d73be8d5d9dd077daaea474512afecad37acda22
Jolene Le <38019858+jolenele@users.noreply.github.com>
2024-10-29 00:25:31 -0700

    Initialized the kotlin mobile app

---
+++ settings.gradle.kts
@@ -0,0 +1,23 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "NPL-Object-Detection-Medical-Bills"
+include(":app")
