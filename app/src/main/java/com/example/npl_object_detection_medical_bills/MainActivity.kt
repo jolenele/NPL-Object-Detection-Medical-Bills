0e0c2c7f99fdef59ec94e418d53a7cc3c50e73e2
Jolene Le <38019858+jolenele@users.noreply.github.com>
2024-12-03 01:28:01 -0800

    Added the Text to Speech feature

--- app/src/main/java/com/example/npl_object_detection_medical_bills/MainActivity.kt
+++ app/src/main/java/com/example/npl_object_detection_medical_bills/MainActivity.kt
@@ -1,27 +1,28 @@
 package com.example.npl_object_detection_medical_bills
 
-import android.os.Bundle
-import android.widget.TextView
-import androidx.activity.enableEdgeToEdge
-import androidx.appcompat.app.AppCompatActivity
-import org.opencv.android.OpenCVLoader
-
 import android.Manifest
 import android.content.pm.PackageManager
-import androidx.core.app.ActivityCompat
-import androidx.core.content.ContextCompat
-
-import org.opencv.android.CameraBridgeViewBase
+import android.graphics.Bitmap
+import android.os.Bundle
+import android.speech.tts.TextToSpeech
+import android.view.View
 import android.widget.Button
 import android.widget.CheckBox
 import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import org.opencv.android.CameraBridgeViewBase
+import org.opencv.android.OpenCVLoader
+import org.opencv.android.Utils
 import org.opencv.core.CvType
 import org.opencv.core.Mat
-
-import android.graphics.Bitmap
-import org.opencv.android.Utils
-
 import org.opencv.imgproc.Imgproc
+import java.util.Locale
+
 
 class MainActivity : AppCompatActivity(), CameraBridgeViewBase.CvCameraViewListener2 {
     private lateinit var textViewStatus: TextView
@@ -30,6 +31,7 @@ class MainActivity : AppCompatActivity(), CameraBridgeViewBase.CvCameraViewListe
     private val cameraPermissionRequestCode = 100
 
     private lateinit var buttonStartPreview: Button
+    private lateinit var buttonTextToSpeech: Button
     private lateinit var buttonStopPreview: Button
     private lateinit var checkBoxProcessing: CheckBox
     private lateinit var imageView: ImageView
@@ -41,6 +43,8 @@ class MainActivity : AppCompatActivity(), CameraBridgeViewBase.CvCameraViewListe
 
     private lateinit var processedMat: Mat
 
+    var t1: TextToSpeech? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -48,6 +52,7 @@ class MainActivity : AppCompatActivity(), CameraBridgeViewBase.CvCameraViewListe
 
         textViewStatus = findViewById(R.id.textViewStatus)
         buttonStartPreview = findViewById(R.id.buttonStartPreview)
+        buttonTextToSpeech = findViewById(R.id.buttonTextToSpeech)
         buttonStopPreview = findViewById(R.id.buttonStopPreview)
         checkBoxProcessing = findViewById(R.id.checkboxEnableProcessing)
         imageView = findViewById(R.id.imageView)
@@ -82,7 +87,30 @@ class MainActivity : AppCompatActivity(), CameraBridgeViewBase.CvCameraViewListe
             updateControls()
         }
 
+        // Text to Speech
+        t1 = TextToSpeech(
+            applicationContext
+        ) { status ->
+            if (status != TextToSpeech.ERROR) {
+                t1!!.setLanguage(Locale.UK)
+            }
+        }
+
+        buttonTextToSpeech.setOnClickListener(View.OnClickListener {
+            val toSpeak: String = textViewStatus.getText().toString()
+            Toast.makeText(applicationContext, toSpeak, Toast.LENGTH_SHORT).show()
+            t1!!.speak(toSpeak, TextToSpeech.QUEUE_FLUSH, null)
+        })
+
         updateControls()
+    }
+
+    public override fun onPause() {
+        if (t1 != null) {
+            t1!!.stop()
+            t1!!.shutdown()
+        }
+        super.onPause()
     }
 
     private fun updateControls() {
